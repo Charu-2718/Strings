305bac169f2d2911f742aa8be8fb3d6d55b3c3d1
Charu-2718 <charu.15421@gmail.com>
2023-08-27 04:36:19 +0530

    reverse

--- ReverseString.cpp
+++ ReverseString.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <algorithm> 
 using namespace std;
 
 int main()
 {
-	string str = "geeksforgeeks";
-	reverse(str.begin(),str.end()); 
-	cout << str << std::endl;
-	return 0;
+    string str;
+    cout << "Type your string: ";
+    getline(cin, str);
+    reverse(str.begin(), str.end());
+    cout << str << std::endl;
+    return 0;
 }
 
 
