f7be99acbd4d1461d892cf97a93b3720d0fc0cb3
Charu-2718 <charu.15421@gmail.com>
2023-09-03 17:35:20 +0530

    sort string

---
+++ new.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm> 
+using namespace std;
+void sortString(string &str)
+{
+sort(str.begin(), str.end());
+cout << str;
+}
+int main()
+{
+	string s = "jjjjjjjggggrcyvgvftyyuvyugty";
+	sortString(s);
+	return 0;
+}
